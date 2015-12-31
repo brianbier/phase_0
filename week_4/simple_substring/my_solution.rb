0af4019f2261c1a8c6b251827ed051f6add2e771
Brian Bier <briansbier@gmail.com>
2015-12-30 20:30:47 -0500

    Simple substring complete

---
+++ week_4/simple_substring/my_solution.rb
@@ -0,0 +1,12 @@
+
+# Simple Substrings
+def welcome(address)
+#An if stament that  will return Welcome to california if the address includes "CA"
+#In order to check the string. I need to use a method to find CA inside the string
+if address.include?("CA")
+return "Welcome to California"
+#ELSE if the statemet does not include CA it should post "You should move to California"
+else 
+return "You should move to California"
+end
+end
