8cb833cc919f0ee7c4ae9f0fec3f00306bdc07c0
Brian Bier <briansbier@gmail.com>
2016-01-03 12:28:32 -0500

    Refactor the triangle question

--- week_4/triangle_side/my_solution.rb
+++ week_4/triangle_side/my_solution.rb
@@ -1,24 +1,24 @@
 # I worked on this challenge [by myself, with: ].
 #WHEN PSUEDO CODING TRY AND WRITE THE PROBLEM YOU ARE TRYING TO SOLVE AND NOT THE CODE YOU NEED TO USE TO SOLVE THE PROBLEM.
 
-def valid_triangle?(int1,int2,int3)
-  #first I need to understand what is an equilateral triangle which is all sides are the same
-  # IF any sides are zero length, it is not a valid triangle
-  if int1 == 0 || int2 == 0 || int3 == 0
-    return false
-  #IF all sides are equal then its an valid Equilateral triangle
-  elsif int1 == int2   && int3 == int1
-    return true
-    #ELSE IF two sides of a triangle are equal lengths than its an valid isosceles triangle
-  elsif int1 == int2 || int1 == int3 || int2 == int3 
-  # #ELSE IF it is pythagorean theorem the square of both the first side and the second side must equal the total of the square of the third side
-    return true
-  elsif int1 ** 2 + int2 ** 2 == int3 ** 2 ||
-          int2 ** 2 + int3 ** 2 == int1 ** 2 ||
-          int3 ** 2 + int1 ** 2 == int2 ** 2
-    return true
-  end
-end
+# def valid_triangle?(int1,int2,int3)
+#   #first I need to understand what is an equilateral triangle which is all sides are the same
+#   # IF any sides are zero length, it is not a valid triangle
+#   if int1 == 0 || int2 == 0 || int3 == 0
+#     return false
+#   #IF all sides are equal then its an valid Equilateral triangle
+#   elsif int1 == int2   && int3 == int1
+#     return true
+#     #ELSE IF two sides of a triangle are equal lengths than its an valid isosceles triangle
+#   elsif int1 == int2 || int1 == int3 || int2 == int3 
+#   # #ELSE IF it is pythagorean theorem the square of both the first side and the second side must equal the total of the square of the third side
+#     return true
+#   elsif int1 ** 2 + int2 ** 2 == int3 ** 2 ||
+#           int2 ** 2 + int3 ** 2 == int1 ** 2 ||
+#           int3 ** 2 + int1 ** 2 == int2 ** 2
+#     return true
+#   end
+# end
 
 # # zero-length tests
 # p valid_triangle?(0,0,0) == false # => false, because a triangle can't have 0-length sides
@@ -38,3 +38,11 @@ end
 
 # # straight up bullshit test
 # p valid_triangle?(10, 10, 100) == false #=> false, no such triangle exists
+
+def valid_triangle?(int1, int2, int3)
+  return false if int1 == 0 || int2 == 0 || int3 == 0
+  return true if int1 == int2 && int1 == int3
+  return true if int1 == int2 || int1 == int3 || int2 == int3
+  return true if int1 ** 2 + int2 ** 2 == int3 **2 || int2 ** 2 + int3 ** 2 == int1 **2 || int3 ** 2 + int1 **2 == int2 **2
+  #inpossible triangle
+end
