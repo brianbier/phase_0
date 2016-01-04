d5dbb3ec2e133033a972a1e3a563ffb3e4cadd06
Brian Bier <briansbier@gmail.com>
2016-01-03 22:55:57 -0500

    Array concat refactor

---
+++ week_4/concatenate_arrays/my_solution.rb
@@ -0,0 +1,17 @@
+# Concatenate Two Arrays
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def array_concat(array_1, array_2)
+  # Your code here
+  # if array_1 == [] && array_2 == []
+  #   return []
+  # else
+  #   array_1 + array_2
+  # end
+  #REFACTOR
+  return array_1.concat(array_2)
+end
