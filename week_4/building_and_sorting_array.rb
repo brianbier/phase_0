cd270fef42b8266a7b82bef0751c5c539e8f3d58
Brian Bier <briansbier@gmail.com>
2016-01-03 20:06:28 -0500

    Modify problem using a loop

--- week_4/building_and_sorting_array.rb
+++ week_4/building_and_sorting_array.rb
@@ -3,19 +3,29 @@
 # all the words in alphabetical order
 
 #Ask how many words we want to type into array
-puts "How many words do you want to include in the array?"
-counter = gets.chomp.to_i
-array = Array.new
-puts ""
-while counter > 0
-puts "Enter desired word:"
-word = gets.chomp
-array.push(word)
-counter -= 1
+# puts "How many words do you want to include in the array?"
+# counter = gets.chomp.to_i
+# words_array = []
+# puts ""
+# until counter == 0
+# puts "Type the desired word:"
+# word = gets.chomp
+# words_array.push(word)
+# counter -= 1
+# end
+# #THEN sort the words in the array in alphabetical order
+# puts 
+# puts words_array.sort
+
+ words_array = []
+while true
+
+ puts "Type the desired word"
+ word = gets.chomp.downcase
+ if word == ""
+ break
+ else
+ words_array.push(word)
+ end
 end
-#THEN sort the words in the array in alphabetical order
-puts 
-puts array.sort
-
-
-
+puts words_array.sort
