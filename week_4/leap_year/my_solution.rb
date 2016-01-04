0c9d81c2be5aa6f3fb78b0a16c7da4625bd98ff2
Brian Bier <briansbier@gmail.com>
2016-01-03 19:17:56 -0500

    Refactor solution

--- week_4/leap_year/my_solution.rb
+++ week_4/leap_year/my_solution.rb
@@ -1,22 +1,38 @@
 # Leap Years
 
-def leap_year?(integer)
+# def leap_year?(integer)
 
-#IF a year is divisable by 4 and has no remainders it is a leap year
-if integer % 4 == 0 && integer % 100 != 0
-  return true
-#ELSE IF a year is divisable by 400 then it is a leap year
-elsif integer % 400 == 0
-  return true
-#ELSE IF a year is divisable by 4 and 100 but not by 400
-elsif integer % 4 == 0 && integer % 100  == 0 && integer % 400 != 0
-  return false
+# #IF a year is divisable by 4 and has no remainders it is a leap year
+# if integer % 4 == 0 && integer % 100 != 0
+#   return true
+# #ELSE IF a year is divisable by 400 then it is a leap year
+# elsif integer % 400 == 0
+#   return true
+# #ELSE IF a year is divisable by 4 and 100 but not by 400
+# elsif integer % 4 == 0 && integer % 100  == 0 && integer % 400 != 0
+#   return false
 
-#ELSE IF a year is not divisable my 4
-elsif integer % 4 != 0
-  return false
+# #ELSE IF a year is not divisable my 4
+# elsif integer % 4 != 0
+#   return false
 
+# end
+# end
+
+def leap_year?(year)
+  return true if year % 4 == 0 && year % 100 != 0
+  return true if year % 400 == 0
+  return false if year % 4 == 0 || year % 100 == 0 || year % 400 != 0
 end
-end
+
+
+
+
+
+
+
+
+
+
 
 
