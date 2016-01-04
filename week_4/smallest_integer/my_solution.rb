39affe40886249c2e083b25385a316e128ca887a
Brian Bier <briansbier@gmail.com>
2016-01-03 21:43:24 -0500

    Smallest integer refactor

--- week_4/smallest_integer/my_solution.rb
+++ week_4/smallest_integer/my_solution.rb
@@ -1,6 +1,3 @@
-mallest Integer
-
-# I worked on this challenge [by myself, with: ].
 
 # smallest_integer is a method that takes an array of integers as its input
 # and returns the smallest integer in the array
@@ -11,6 +8,41 @@ mallest Integer
 # If +list_of_nums+ is empty the method should return nil
 
 # Your Solution Below
-def smallest_integer(list_of_nums)
-  # Your code goes here!
+#PSUEDO CODE
+
+#Need to identify the smallest integer inside an array
+#I will have to sort the array from smallest to greatest
+#THEN I will input the first idex of the array
+
+def smallest_integer(number)
+  # return nil if number == []
+  # if number.join().to_i == 0
+  #   return 0
+  # else
+  #   lowest_number = number.sort
+  #   return lowest_number[0]
+  # end
+#REFACTOR
+    lowest_number = number.sort
+    return lowest_number[0]
 end
+
+
+# smallest_integer([-10, 0, 10])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
