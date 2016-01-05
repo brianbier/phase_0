a9f5172a774422fc71c527727b2e5db92cfb1032
Brian Bier <briansbier@gmail.com>
2016-01-05 13:25:48 -0500

    Refactor factorial number

--- week_4/factorial/my_solution.rb
+++ week_4/factorial/my_solution.rb
@@ -1,36 +1,70 @@
-# Factorial
+# # Factorial
 
-#Your Solution Below
+# #Your Solution Below
+# def factorial(number)
+#   # satisfy condition where 0! = 1
+#   if number == 0
+#     1
+#   # satisfy condition where number > 0
+#   else
+#     # make an array to store all numbers from 1 --> number
+#     # array = (1..number)
+#     # start = 0
+#     # while start < number do
+#     #   start = start + 1
+#     #   array.push(start)
+#     # end
+
+#     # make a new array to store products
+    
+
+#     # initialize product at 1
+#     product = 1
+
+#     # for each number in array, store the product as (product * number)
+#     # and push the new product to new_array
+#     (1..number).each do |num|
+#       product = product * num
+      
+#     end
+
+#     # return the integer stored in the last index of new_array
+#     product
+#   end
+# end
+#REFACTOR
+
 def factorial(number)
-  # satisfy condition where 0! = 1
-  if number == 0
-    1
-  # satisfy condition where number > 0
-  else
-    # make an array to store all numbers from 1 --> number
-    array = []
-    start = 0
-    while start < number do
-      start += 1
-      array.push(start)
-    end
-
-    # make a new array to store products
-    new_array = []
-
-    # initialize product at 1
-    product = 1
-
-    # for each number in array, store the product as (product * number)
-    # and push the new product to new_array
-    array.each do |num|
-      product *= num
-      new_array.push(product)
-    end
-
-    # return the integer stored in the last index of new_array
-    return new_array[-1]
-  end
+#Return 1 for any number that is 0
+return 1 if number == 0 
+# This is where the new number will be store
+total = 1
+# Create a range and iterate through each number and then assign that number to total
+(1..number).each do |num|
+  total = total * num
+end
+# return total so it can display the last number.
+total
 end
 
-factorial(10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
