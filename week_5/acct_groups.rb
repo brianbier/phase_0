83823f6beacc159d8c06d31a5c830b271760a64c
Brian Bier <briansbier@gmail.com>
2016-01-10 23:20:11 -0500

    Refactoring complete

--- week_5/acct_groups.rb
+++ week_5/acct_groups.rb
@@ -1,29 +1,51 @@
-def groups(list_of_names)
-  group_list = { "group1"=> [], "group2" => [],"group3" =>[],"group4"=>[]}
+# def groups(list_of_names)
+#   group_list = { "group1"=> [], "group2" => [],"group3" =>[],"group4"=>[]}
 
-  list_of_names.shuffle.each do |name|
+#   list_of_names.shuffle.each do |name|
 
-    if group_list["group1"].length < 5
-      group_list["group1"].push(name)
-    elsif group_list["group2"].length < 5
-      group_list["group2"].push(name)
-    elsif group_list["group3"].length < 5
-      group_list["group3"].push(name)
-    else
-      group_list["group4"].push(name)
+#     if group_list["group1"].length < 5
+#       group_list["group1"].push(name)
+#     elsif group_list["group2"].length < 5
+#       group_list["group2"].push(name)
+#     elsif group_list["group3"].length < 5
+#       group_list["group3"].push(name)
+#     else
+#       group_list["group4"].push(name)
         
+#     end
+#   end
+#   puts group_list['group1']
+#   puts ""
+#   puts group_list['group2']
+#   puts ""
+#   puts group_list['group3']
+#   puts ""
+#   puts group_list['group4']
+# end
+
+# names = ["Brian Bier","Angela Flaquer","Genesis Bier","John Schwarts","Ana Garcia","Justin Bias","Alex Rodriguez","German Bier","Danelly Torres","David Woodruff","Jimmy Duraku","Scott black","Walter white","Brendan Smith","Frank Delpideo",]
+
+
+# groups(names)
+
+#REFACTOR SOLUTION
+
+def groups(list_of_names)
+  puts list_of_names.length
+  if list_of_names.length < 3 || list_of_names.length % 5 == 0
+     group_list = list_of_names.shuffle.each_slice(5).to_a
+     number = 1
+     group_list.each do |name|
+      puts "Group #{number}"
+      puts name
+      puts "-------------"
+      number += 1
     end
+  else
+     puts "That is not an even number."
   end
-  puts group_list['group1']
-  puts ""
-  puts group_list['group2']
-  puts ""
-  puts group_list['group3']
-  puts ""
-  puts group_list['group4']
+
 end
 
-names = ["Brian Bier","Angela Flaquer","Genesis Bier","John Schwarts","Ana Garcia","Justin Bias","Alex Rodriguez","German Bier","Danelly Torres","David Woodruff","Jimmy Duraku","Scott black","Walter white","Brendan Smith","Frank Delpideo",]
-
-
+names = ["Brian Bier","Angela Flaquer","Genesis Bier","John Schwarts","Ana Garcia","Justin Bias","Alex Rodriguez","German Bier","Danelly Torres","David Woodruff","Jimmy Duraku","Scott black","Walter white","Brendan Smith","Frank Delpideo"]
 groups(names)
