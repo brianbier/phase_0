ca6c3bc51eb0146fbfd4ec6859e3433ef84dfe09
Brian Bier <briansbier@gmail.com>
2016-01-13 16:13:42 -0500

    Reflection 2 complete

--- week_6/attr/my_solution.rb
+++ week_6/attr/my_solution.rb
@@ -24,20 +24,33 @@ end
 
 # Reflection
 =begin
-Release 1 
+# Release 1 
 
 What are these methods doing?
 These methods give the instance variables the ability to be displayed and be re-assigned.outside of the class.
 
 How are they modifying or returning the value of instance variables?
 
-They are modifying the variables with the methods that have the equal sign for example: change_my_name=(new_name) is the method that assigns the new value for name.
+They are modifying the variables with the methods that have the equal sign for example: change_my_name=(new_name) 
+is the method that assigns the new value for name.
 
 They are returning the instance variable names with the methods that do not take an argument and they only have the instance name within the method. 
   Example: 
   def what_is_name 
     @name
   end
+
+# Release 2
+
+What changed between the last release and this release?
+They eliminated the method what_is_age for an attr_reader :age
+
+What was replaced? 
+They replace the the method for the attr_reader :age
+
+Is this code simpler than the last?
+It is not simpler but with practice you begin to remember what attr_reader will do
+What I notice is that you write less code.
 =end
 
 
