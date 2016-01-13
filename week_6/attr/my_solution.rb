9f88056a1ed24f034c857a87b86c2242d7bf4a42
Brian Bier <briansbier@gmail.com>
2016-01-13 16:07:52 -0500

    Release 1 reflection complete

---
+++ week_6/attr/my_solution.rb
@@ -0,0 +1,43 @@
+
+#Attr Methods
+
+# I worked on this challenge [by myself, with:]
+
+# I spent [#] hours on this challenge.
+
+# Pseudocode
+
+# Input:
+# Output:
+# Steps:
+
+class NameData
+
+end
+
+
+class Greetings
+
+end
+
+
+
+# Reflection
+=begin
+Release 1 
+
+What are these methods doing?
+These methods give the instance variables the ability to be displayed and be re-assigned.outside of the class.
+
+How are they modifying or returning the value of instance variables?
+
+They are modifying the variables with the methods that have the equal sign for example: change_my_name=(new_name) is the method that assigns the new value for name.
+
+They are returning the instance variable names with the methods that do not take an argument and they only have the instance name within the method. 
+  Example: 
+  def what_is_name 
+    @name
+  end
+=end
+
+
