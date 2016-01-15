69cdc74312fc2ac2eb288330aa3787452fa5640c
Brian Bier <briansbier@gmail.com>
2016-01-14 19:45:08 -0500

    Initial solo challenge complete

--- week_6/bingo_solution.rb
+++ week_6/bingo_solution.rb
@@ -1,38 +1,69 @@
 # A Nested Array to Model a Bingo Board SOLO CHALLENGE
 
-# I spent [#] hours on this challenge.
-
 
 # Release 0: Pseudocode
 # Outline:
 
 # Create a method to generate a letter ( b, i, n, g, o) and a number (1-100)
-  #fill in the outline here
+  # create variable that will hold any random letter in the word bingo
+  # Create a variable that will hold one random number
 
 # Check the called column for the number called.
-  #fill in the outline here
+  # Iterate through the board game to find the number within the arrays
 
 # If the number is in the column, replace with an 'x'
-  #fill in the outline here
-
-# Display a column to the console
-  #fill in the outline here
+  #IF the the column has the random number it will replace it with an X
 
 # Display the board to the console (prettily)
-  #fill in the outline here
+  #Create a method that will display the entire board game into the console
 
 # Initial Solution
 
+# class BingoBoard
+#     attr_reader :number, :letter_position, :letters
+
+#   def initialize(board)
+#     @bingo_board = board
+#   end
+
+#   def call
+#     @letters = ["B","I","N","G","O"]
+#     random_letter = ["B","I","N","G","O"].sample
+#     @letter_position = letters.index(random_letter)
+#     @number = 75
+#     @combination = "#{random_letter}:#{number.to_s}" 
+#   end
+
+#   def check
+#     @bingo_board.each do |array|
+#       if array.is_a?(Array)
+#          if array[@letter_position] == @number
+#           array[@letter_position] = " X"
+#         else
+#           array[@letter_position]
+#         end
+#     end
+#   end
+#  end
+
+#   def print
+#     puts "Lets Play Bingo!"
+#     puts ""
+#     p "First Set is " + @combination
+#     puts ""
+#     puts "#{@bingo_board[0].join("|")}\n#{@bingo_board[1].join("|")}\n#{@bingo_board[2].join("|")}\n#{@bingo_board[3].join("|")}\n#{@bingo_board[4].join("|")}"
+#   end
+
+# end
+
+# Refactored Solution
 class BingoBoard
 
   def initialize(board)
     @bingo_board = board
   end
 
-
 end
-
-# Refactored Solution
 
 
 
@@ -43,7 +74,12 @@ board = [[47, 44, 71, 8, 88],
         [25, 31, 96, 68, 51],
         [75, 70, 54, 80, 83]]
 
-new_game = BingoBoard.new(board)
+p new_game = BingoBoard.new(board)
+# new_game.call
+# new_game.check
+# puts ""
+# new_game.print
+
 
 
 #Reflection
