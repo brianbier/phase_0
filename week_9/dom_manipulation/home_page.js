8d3f015924ece86957bf27332558435faffae78e
Brian Bier <briansbier@gmail.com>
2016-02-03 16:36:44 -0500

    Include comments for each line of code

--- week_9/dom_manipulation/home_page.js
+++ week_9/dom_manipulation/home_page.js
@@ -1,33 +1,29 @@
 // DOM Manipulation Challenge
 
-
-// I worked on this challenge [by myself, with: ].
-
-
 // Add your JavaScript calls to this page:
 
 // Release 1:
-
+// ADD A CLASS NAME WITH VALUE
 document.getElementById('release-0').className += 'done';
 
 
 // Release 2:
-
+// STYLE AN ID
 document.getElementById('release-1').style.display = 'none';
 
 
 // Release 3:
-
+// INCLUDE TEXT INSIDE AN HTML ELEMENT [0] IS TARGETING THE FIRST
 document.getElementsByTagName('h1')[0].innerHTML = 'I completed release 2.';
 
 
 // Release 4:
-
+//STYLE THE ELEMENT ID
 document.getElementById('release-3').style.background = '#955251';
 
 
 // Release 5:
-
+//ITERATE IN ORDER TO TARGET EVERY CLASS AND STYLE IT 
 var changeFontSize = document.getElementsByClassName('release-4');
 for(var i = 0;i < changeFontSize.length; i++){
   changeFontSize[i].style.fontSize = '2em'
@@ -36,7 +32,7 @@ for(var i = 0;i < changeFontSize.length; i++){
 // Release 6:
 
 var template = document.getElementById('hidden');
-
+//ACTIVATE THE TEMPLATE AND CLONE IT SO THAT IT CAN BE DISPLAY INTO BODY USING APPEND CHILD
 document.body.appendChild(template.content.cloneNode(true));
 
 
